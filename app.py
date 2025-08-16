9f898df1fe0805ab3d71cdf11da713b51e935ff9
Rafael Gelde <rafaelgelde@gmail.com>
2025-08-16 09:21:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,3 @@ app = FastAPI()
 @app.get("/")
 def read_root():
  return {"message": "Olá turma, este é meu primeiro container com FastAPI!"}
-
-@app.get("/Rafael")
-def read_root():
- return {"message": "Olá turma, ete é o metodo do Rafael"}
